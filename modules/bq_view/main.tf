9d553f61859455e30f9990e0c5cfddc4b87cfe6c
Arnaud Verbeke <arnaud.verbeke@devoteamgcloud.com>
2024-07-09 15:18:07 +0200

    update module bq_view

--- modules/bq_view/main.tf
+++ modules/bq_view/main.tf
@@ -1,16 +1,3 @@
-resource "google_bigquery_dataset" "view_dataset" {
-  dataset_id                  = var.view_dataset_id
-  project                     = var.project_id
-  friendly_name               = var.view_dataset_id
-  description                 = "view dataset"
-  location                    = "EU"
-  default_table_expiration_ms = 3600000
-
-  labels = {
-    env = terraform.workspace
-  }
-}
-
 resource "google_bigquery_table" "target_view" {
     dataset_id          = var.view_dataset_id
     table_id            = var.view_id
---
+++ modules/bq_view/outputs.tf
@@ -0,0 +1,3 @@
+output "target_view" {
+    value = resource.target_view
+}
